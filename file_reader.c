8680ed3a3048eea160f57db03d4871360587889e
Przemek Dudek <przemod77@gmail.com>
2023-01-16 23:56:39 +0100

    Po interwencji Matiego

--- file_reader.c
+++ file_reader.c
@@ -16,15 +16,15 @@ struct disk_t* disk_open_from_file(const char* volume_file_name)
         return NULL;
     }
 
-    struct disk_t *disc = calloc(1, sizeof(struct disk_t));
-    if(disc == NULL) {
+    struct disk_t *disk = calloc(1, sizeof(struct disk_t));
+    if(disk == NULL) {
         errno = ENOMEM;
         return NULL;
     }
 
-    disc->f = f;
+    disk->f = f;
 
-    return disc;
+    return disk;
 }
 
 
@@ -56,16 +56,52 @@ int disk_close(struct disk_t* pdisk)
         fclose(pdisk->f);
     }
 
-    if(pdisk->opened) {
-        free(pdisk->opened);
-    }
-
     free(pdisk);
 
     return 0;
 }
 
 //##############################################################################################
+
+int checkSuperSector(struct fat_super_t *superSector)
+{
+    if(superSector->bytes_per_sector != 512) {
+        return 1;
+    }
+
+    if(superSector->fat_count != 2) {
+        return 1;
+    }
+
+    if(superSector->magic != 0xaa55) {
+        return 1;
+    }
+
+    return 0;
+}
+
+int fat_compare(struct volume_t *volume, struct disk_t* pdisk, uint32_t first_sector)
+{
+    char buff_FAT1[512], buff_FAT2[512];
+
+    for(int i = 0; i < volume->FAT_sectors; i++) {
+        if(disk_read(pdisk, first_sector + volume->FAT1_Pos + i, buff_FAT1, 1) == -1) {
+            return 1;
+        }
+
+        if(disk_read(pdisk, first_sector + volume->FAT2_Pos + i, buff_FAT2, 1) == -1) {
+            return 1;
+        }
+
+        for(int j = 0; j < 512; j++) {
+            if(buff_FAT1[j] != buff_FAT2[j]) {
+                return 1;
+            }
+        }
+    }
+
+    return 0;
+}
 
 struct volume_t* fat_open(struct disk_t* pdisk, uint32_t first_sector)
 {
@@ -80,10 +116,57 @@ struct volume_t* fat_open(struct disk_t* pdisk, uint32_t first_sector)
         return NULL;
     }
 
-    volume->disk = pdisk;
-    pdisk->opened = volume;
+    char *buffer = malloc(512);
+    if(buffer == NULL) {
+        errno = ENOMEM;
+        free(volume);
+        return NULL;
+    }
 
-    if(disk_read(pdisk, first_sector, NULL, 1) == -1) {
+    if(disk_read(pdisk, first_sector, buffer, 1) == -1) {
+        errno = EINVAL;
+        free(buffer);
+        free(volume);
+        return NULL;
+    }
+
+    struct fat_super_t *superSector = (struct fat_super_t*)buffer;
+
+    if(checkSuperSector(superSector)) {
+        free(buffer);
+        free(volume);
+        errno = EINVAL;
+        return NULL;
+    }
+
+    volume->disk = pdisk;
+
+    volume->SuperSector = superSector;
+    volume->FAT1_Pos = superSector->reserved_sectors;
+    volume->FAT2_Pos = superSector->reserved_sectors + superSector->sectors_per_fat;
+    volume->FAT_sectors = superSector->sectors_per_fat;
+
+    volume->ROOT_Dir_Pos = superSector->reserved_sectors + superSector->sectors_per_fat*superSector->fat_count;
+    volume->ROOT_Sectors = (superSector->root_dir_capacity * sizeof(struct fat_entry_t))/superSector->bytes_per_sector;
+    if((superSector->root_dir_capacity * sizeof(struct fat_entry_t)) % superSector->bytes_per_sector) {
+        volume->ROOT_Sectors += 1;
+    }
+
+    volume->volume_size = superSector->logical_sectors16 == 0 ? superSector->logical_sectors32 : superSector->logical_sectors16;
+    volume->user_size = volume->volume_size - (superSector->fat_count * superSector->sectors_per_fat) - superSector->reserved_sectors - volume->ROOT_Sectors;
+    volume->number_of_cluster_per_volume = volume->user_size / superSector->sectors_per_cluster;
+    if(volume->user_size % superSector->sectors_per_cluster) {
+        volume->number_of_cluster_per_volume += 1;
+    }
+
+    volume->cluster2_position = volume->ROOT_Dir_Pos + volume->ROOT_Sectors;
+
+
+    volume->DATA_Pos = volume->ROOT_Dir_Pos + (superSector->root_dir_capacity*32)/512;
+
+    if(fat_compare(volume, pdisk, first_sector)) {
+        free(buffer);
+        free(volume);
         errno = EINVAL;
         return NULL;
     }
@@ -99,8 +182,6 @@ int fat_close(struct volume_t* pvolume)
         return -1;
     }
 
-
-
     free(pvolume);
 
     return 0;
@@ -108,13 +189,90 @@ int fat_close(struct volume_t* pvolume)
 
 //##############################################################################################
 
+int is_valid(char n)
+{
+    if((n >= 'A' && n <= 'Z') || (n >= 'a' && n <= 'z')) {
+        return 1;
+    }
+
+    if((n >= '0' && n <= '9') || n == '_' || n == -27) {
+        return 1;
+    }
+
+    return 0;
+}
+
 struct file_t* file_open(struct volume_t* pvolume, const char* file_name)
 {
     if(pvolume == NULL || file_name == NULL) {
         return NULL;
     }
 
-    return NULL;
+    struct file_t *file = calloc(1, sizeof(struct file_t));
+    if(file == NULL) {
+        errno = ENOMEM;
+        return NULL;
+    }
+
+    file->vol = pvolume;
+
+    char *buff = calloc(file->vol->ROOT_Sectors, 512);
+    if(buff == NULL) {
+        free(file);
+        errno = ENOMEM;
+        return NULL;
+    }
+
+    if(disk_read(file->vol->disk, (uint16_t)(pvolume->FAT2_Pos + pvolume->FAT_sectors), buff, file->vol->ROOT_Sectors) == -1) {
+        free(file);
+        free(buff);
+        errno = ENOMEM;
+        return NULL;
+    }
+
+
+    struct fat_entry_t *entry;
+
+    for(int i = 0; i < 24; i++) {
+        entry = (struct fat_entry_t*)(buff+i*32);
+
+        char tmp[13];
+
+        for(int j = 0; j < 13; j++) {
+            tmp[j] = '\0';
+        }
+
+        for(int j = 0; j < (int)strlen(entry->name); j++) {
+            if(!is_valid(*(entry->name + j))) {
+                break;
+            }
+
+            tmp[j] = *(entry->name + j);
+        }
+
+        if(is_valid(*(entry->extenstion))) {
+            tmp[strlen(tmp)] = '.';
+        }
+
+        int name_len = strlen(tmp);
+
+        for(int j = 0; j < (int)strlen(entry->extenstion); j++) {
+            if(!is_valid(*(entry->extenstion + j))) {
+                break;
+            }
+
+            tmp[j+name_len] = *(entry->extenstion + j);
+        }
+
+        if(strcmp(tmp, file_name) == 0) {
+            break;
+        }
+    }
+
+
+    file->entry = entry;
+
+    return file;
 }
 
 
@@ -123,6 +281,12 @@ int file_close(struct file_t* stream)
     if(stream == NULL) {
         return -1;
     }
+
+    if(stream->entry != NULL) {
+        free(stream->entry);
+    }
+
+    free(stream);
 
     return 0;
 }
@@ -134,7 +298,54 @@ size_t file_read(void *ptr, size_t size, size_t nmemb, struct file_t *stream)
         return -1;
     }
 
-    return 1;
+    uint16_t *buffer = (uint16_t*)malloc((stream->vol->number_of_cluster_per_volume + 2) * sizeof(int));
+    if(buffer == NULL) {
+        errno = ENOMEM;
+        return -1;
+    }
+
+    int *fat1_data = calloc(stream->vol->SuperSector->sectors_per_fat*stream->vol->SuperSector->bytes_per_sector, 1);
+    if(fat1_data == NULL) {
+        free(buffer);
+        errno = ENOMEM;
+        return -1;
+    }
+
+    if(disk_read(stream->vol->disk, stream->vol->FAT1_Pos, fat1_data, stream->vol->SuperSector->sectors_per_fat) == -1) {
+        free(buffer);
+        free(fat1_data);
+        return -1;
+    }
+
+    for(int i = 0, j = 0; i < stream->vol->number_of_cluster_per_volume + 2; i += 2, j += 3) {
+        uint8_t b1 = fat1_data[j];
+        uint8_t b2 = fat1_data[j + 1];
+        uint8_t b3 = fat1_data[j + 2];
+
+        int c1 = ((b2 & 0x0F) << 8) | b1;
+        int c2 = ((b2 & 0xF0) >> 4) | (b3 << 4);
+        buffer[i] = c1;
+        buffer[i + 1] = c2;
+    }
+
+    uint16_t table[512];
+
+    uint16_t val = stream->entry->low_cluster_index;
+
+    for(int i = 0; val != *buffer; i++) {
+        table[i] = val;
+        val = buffer[val];
+
+        if(disk_read(stream->vol->disk, (table[i] - 2) * stream->vol->SuperSector->sectors_per_cluster + stream->vol->DATA_Pos, (char*)ptr + i * stream->vol->SuperSector->sectors_per_cluster, stream->vol->SuperSector->sectors_per_cluster) == -1) {
+            return -1;
+        }
+    }
+
+
+
+
+
+    return nmemb*size;
 }
 
 
--- file_reader.h
+++ file_reader.h
@@ -4,14 +4,30 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <errno.h>
+#include <string.h>
 
 struct disk_t {
     FILE *f;
-    struct volume_t *opened;
 };
 
 struct volume_t {
-    struct disk_t* disk;
+    struct fat_super_t *SuperSector;
+    struct disk_t *disk;
+
+    int FAT1_Pos;
+    int FAT2_Pos;
+    int FAT_sectors;
+
+    int ROOT_Dir_Pos;
+    int ROOT_Sectors;
+
+    int volume_size;
+    int user_size;
+    int number_of_cluster_per_volume;
+
+    int cluster2_position;
+
+    int DATA_Pos;
 };
 
 struct time_godziny_t{
@@ -36,26 +52,26 @@ enum atribute_t{
     lfn = 0x0F
 };
 
-struct file_description_t {
-    char nazwa[8];
-    char rozszerzenie[3];
-    enum atribute_t atribute;   //uint8
-    uint8_t isReserved;
-    uint8_t createTime;
-    struct time_godziny_t godzina;
-    struct time_dzien_t dzien;
-    uint16_t lastTime;
-    uint16_t first_cluster_older;
-    struct time_godziny_t lastModeHour;
-    struct time_dzien_t lastModeDate;
-    uint16_t first_cluster_younger;
-    uint32_t size;
-};
+struct fat_entry_t {
+    char name[8];
+    char extenstion[3];
+    uint8_t attributes;
+
+    uint8_t __some_data[6];
+    uint16_t __some_data2;
+
+    uint16_t high_cluster_index;
+    uint16_t __some_data3[2];
+    uint16_t low_cluster_index;
+    uint32_t file_size;
+
+} __attribute__(( packed ));
+
 
 struct file_t{
     struct volume_t* vol;
     struct cluster_t* clus;
-    struct file_description_t description;
+    struct fat_entry_t *entry;
     uint16_t cluster_number;
     uint16_t file_position;
 };
@@ -79,6 +95,36 @@ struct dir_entry_t {
     int is_hidden;
     int is_directory;
 };
+
+struct fat_super_t {
+    uint8_t __jump_code[3];
+    char oem_name[8];
+
+    uint16_t bytes_per_sector;
+    uint8_t sectors_per_cluster;
+    uint16_t reserved_sectors;
+    uint8_t fat_count;
+    uint16_t root_dir_capacity;
+    uint16_t logical_sectors16;
+    uint8_t __reserved;
+    uint16_t sectors_per_fat;
+
+    uint32_t __reserved2;
+
+    uint32_t hidden_sectors;
+    uint32_t logical_sectors32;
+
+    uint16_t __reserved3;
+    uint8_t __reserved4;
+
+    uint32_t serial_number;
+
+    char label[11];
+    char fsid[8];
+
+    uint8_t __boot_code[448];
+    uint16_t magic;
+} __attribute__(( packed ));
 
 struct disk_t* disk_open_from_file(const char* volume_file_name);
 int disk_read(struct disk_t* pdisk, int32_t first_sector, void* buffer, int32_t sectors_to_read);
